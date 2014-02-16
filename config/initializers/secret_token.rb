6d9ac30ea0792cf7354b92a66c37cf5b33c0b402
anisharamnani <anisha.ramnani@gmail.com>
2014-02-16 16:24:20 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BFFGroove::Application.config.secret_token = 'c63c557613200a22dddb463e32cac8f6b23827a25c7cfc2df54a4ecfbc3d908a0854dbd336ac06b7bf59eae152f5bf7374cafba198c1914f86fcd87f44e7739c'
