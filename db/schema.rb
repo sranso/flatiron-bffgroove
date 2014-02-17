31416dc43398d9337b9f27c859513482b2e34a11
Pooja Pasawala <pooja@basicer.com>
2014-02-17 16:37:31 -0500

    Fix groupaign table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140217200922) do
+ActiveRecord::Schema.define(:version => 20140217213530) do
 
   create_table "campaigns", :force => true do |t|
     t.string   "title"
@@ -58,8 +58,10 @@ ActiveRecord::Schema.define(:version => 20140217200922) do
 
   create_table "groupaigns", :force => true do |t|
     t.string   "title"
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
+    t.string   "subject"
+    t.string   "list"
+    t.datetime "send_date"
+    t.string   "send_weekday"
     t.integer  "total_recipients"
     t.integer  "successful_deliveries"
     t.integer  "soft_bounces"
@@ -68,16 +70,32 @@ ActiveRecord::Schema.define(:version => 20140217200922) do
     t.integer  "times_forwarded"
     t.integer  "forwarded_opens"
     t.integer  "unique_opens"
+    t.decimal  "open_rate"
     t.integer  "total_opens"
     t.integer  "unique_clicks"
+    t.decimal  "click_rate"
     t.integer  "total_clicks"
     t.integer  "unsubscribes"
     t.integer  "abuse_complaints"
     t.integer  "times_liked_on_facebook"
     t.integer  "folder_id"
+    t.string   "unique_id"
+    t.decimal  "analytics_ROI"
+    t.decimal  "campaign_cost"
+    t.decimal  "revenue_created"
     t.integer  "visits"
     t.integer  "new_visits"
+    t.float    "pages_visit"
+    t.decimal  "bounce_rate"
+    t.time     "time_on_site"
+    t.decimal  "goal_conversion_rate"
+    t.decimal  "per_visit_goal_value"
     t.integer  "transactions"
+    t.decimal  "ecommerce_conversion_rate"
+    t.decimal  "per_visit_value"
+    t.decimal  "average_value"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
 end
