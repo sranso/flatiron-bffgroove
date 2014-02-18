3cde7941d6d15857bf5ef8f4bd6408afd430b458
anisharamnani <anisha.ramnani@gmail.com>
2014-02-18 10:31:19 -0500

    toggle display of campaigns table

--- public/actions.js
+++ public/actions.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   $(".show_button").on("click", function(e) {
     e.preventDefault();
-    
+    $(".campaigns_table").toggleClass("display_table");
+    if($(".campaigns_table").hasClass("display_table")){
+      $(this).text("Hide Campaigns");
+    } else {
+      $(this).text("Show Campaigns");
+    }
   });
 });
