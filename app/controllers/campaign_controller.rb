0c73204300149cd891ad0c00422e17bfa977e556
sranso <sranso@gmail.com>
2014-02-19 15:35:01 -0500

    add some of the js, show json on pages

--- app/controllers/campaign_controller.rb
+++ app/controllers/campaign_controller.rb
@@ -3,5 +3,10 @@ class CampaignController < ApplicationController
 
   def index
     @campaigns = Campaign.first(200)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @campaigns }
+    end
   end
 end
