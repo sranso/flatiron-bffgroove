e84fab4749c5d3dd157c6f58c081763522855a48
sranso <sranso@gmail.com>
2014-02-19 10:03:32 -0500

    fix devise for real this time bang

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 BFFGroove::Application.routes.draw do
 
-  devise_for :users
+  # devise
+  devise_for :users, :skip => [:sessions, :registerable]
+  as :user do
+    get '/users/sign_in' => 'devise/sessions#new', :as => :new_user_session
+    post '/users/sign_in' => 'devise/sessions#create', :as => :user_session
+    delete '/users/sign_out' => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+  get '/users/sign_up' => 'campaign#home'
 
+  # other routes
   root :to => 'campaign#home'
   get '/campaigns' => 'campaign#index'
   get '/group_campaigns'=> 'group_campaign#index'
