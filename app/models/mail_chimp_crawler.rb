39c8d41ffe9a025c4586d306d2e8c924fd5fb237
sranso <sranso@gmail.com>
2014-02-25 13:18:21 -0500

    huh

--- app/models/mail_chimp_crawler.rb
+++ app/models/mail_chimp_crawler.rb
@@ -9,9 +9,8 @@ class MailChimpCrawler
     HTTParty.get("https://us5.api.mailchimp.com/2.0/reports/google-analytics?apikey=#{ENV['API_KEY']}&cid=#{campaign_id}")
   end
 
-  def list_name(list_id)
-    response = HTTParty.get("https://us5.api.mailchimp.com/2.0/lists/list?apikey=#{ENV['API_KEY']}&filters[list_id]=#{list_id}")
-    response["data"][0]["name"]
+  def all_lists
+    HTTParty.get("https://us5.api.mailchimp.com/2.0/lists/list?apikey=#{ENV['API_KEY']}&limit=100")
   end
 
 end
