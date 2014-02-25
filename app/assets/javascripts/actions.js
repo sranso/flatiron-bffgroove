13e6057b8e819e5a887b0629a339a7077a29a8ce
anisharamnani <anisha.ramnani@gmail.com>
2014-02-25 15:34:28 -0500

    add link to the groupCampaign handsontable

--- app/assets/javascripts/actions.js
+++ app/assets/javascripts/actions.js
@@ -1,5 +1,36 @@
 var data1;
 $(document).ready(function() {
+
+  function strip_tags(input, allowed){
+    allowed = (((allowed || "") + ""). toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join('');
+    var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi, commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
+    return input.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
+      return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';
+    });
+  }
+
+  var descriptionRenderer = function (instance, td, row, col, prop, value, cellProperties) {
+    var escaped = Handsontable.helper.stringify(value);
+    escaped = strip_tags(escaped, '<em><b><a>'); //be sure you only allow certain HTML tags to avoid XSS threats (you should also remove unwanted HTML attributes)
+    td.innerHTML = escaped;
+    return td;
+  };
+
+  var coverRenderer = function (instance, td, row, col, prop, value, cellProperties) {
+    var escaped = Handsontable.helper.stringify(value);
+    if (escaped.indexOf('http') === 0) {
+      var $img = $('<img>');
+      $img.attr('src', value);
+      $img.on('mousedown', function (event) {
+        event.preventDefault(); //prevent selection quirk
+      });
+      $(td).empty().append($img); //empty is needed because you are rendering to an existing cell
+    }
+    else {
+      Handsontable.renderers.TextRenderer.apply(this, arguments); //render as text
+    }
+    return td;
+  };
   $(".show_button").on("click", function(e) {
     $(".tableGCCampaigns").toggleClass("hide_table");
     if ($(".tableGCCampaigns").hasClass("hide_table")) {
@@ -110,36 +141,42 @@ $(document).ready(function() {
     }
   });
 
-$.ajax('/group_campaigns.json', {
-  type: 'GET', 
-  success: function(data){
-    data1 = data; 
-    var dataResponse = data; 
-    var keysGroupCampaigns = []; 
+  $.ajax('/group_campaigns.json', {
+    type: 'GET', 
+    success: function(data){
+      data1 = data; 
+      var dataResponse = data; 
+      var keysGroupCampaigns = []; 
 
-    for (var k in dataResponse[0]){
-      kNew = replaceAll("_", " ", k).toUpperCase(); 
-      keysGroupCampaigns.push(kNew);
+      for (var k in dataResponse[0]){
+        kNew = replaceAll("_", " ", k).toUpperCase(); 
+        keysGroupCampaigns.push(kNew);
+      }
+      makeGroupCampaignsTable(".tableGroupCampaigns", dataResponse, keysGroupCampaigns);
+      makeSortable(".tableGroupCampaigns");
+    }, 
+
+    error: function(data){
+      console.log("Error with the fetch")
     }
-    makeGroupCampaignsTable(".tableGroupCampaigns", dataResponse, keysGroupCampaigns);
-    makeSortable(".tableGroupCampaigns");
-  }, 
+  })
 
-  error: function(data){
-    console.log("Error with the fetch")
+  function makeGroupCampaignsTable(div, tableData, keys){
+    $(div).handsontable({
+          data: tableData,
+          colHeaders: keys,
+          readOnly: true,
+          columnSorting: true,
+          persistentState: true,
+          manualColumnMove: true,
+          manualColumnResize: true,
+          contextMenu: true,
+          columns: [
+            {data: "title", renderer: descriptionRenderer}, 
+            {data: "send_date"} 
+          ]
+        });
   }
-})
 
-function makeGroupCampaignsTable(div, tableData, keys){
-  $(div).handsontable({
-        data: tableData,
-        colHeaders: keys,
-        readOnly: true,
-        columnSorting: true,
-        persistentState: true,
-        manualColumnMove: true,
-        manualColumnResize: true,
-        contextMenu: true
-      });
-}
 });
+
