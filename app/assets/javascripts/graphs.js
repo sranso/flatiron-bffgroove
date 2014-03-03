19f40f2f1573c3f5af37723af70baa60267cd244
sranso <sranso@gmail.com>
2014-03-03 12:14:19 -0500

    one day populates on graph

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -42,23 +42,22 @@ $(document).ready(function() {
     d3.json("/group_campaigns/graph.json", function(error, data) {
       // returns keys, makes them for side bar
       response = data;
-      var campaignNames = d3.keys(data[0]).filter(function(key) { return key !== "weekday"; }));
+      var campaignNames = d3.keys(data[0]).filter(function(key) { return key !== "weekday"; });
       // grouping each of the bars
-      debugger
       response.forEach(function(d) {
         d.days = campaignNames.map(function(name) {
+          // debugger
           return {name: name, value: +d[name]};
         });
       });
       
       // labels for x axis
-      x0.domain(response.map(function(d) { return d.send_weekday; }));
+      x0.domain(response.map(function(d) { return d.weekday; }));
       // huh?
       x1.domain(campaignNames).rangeRoundBands([0, x0.rangeBand()]);
       // set scale of y axis
       y.domain([0, d3.max(response, function(d) {
-        
-        return +d[name]; })]);
+        return d3.max(d.days, function(d) { return d.value;})})]);
 
       svg.append("g")
           .attr("class", "x axis")
@@ -79,7 +78,7 @@ $(document).ready(function() {
           .data(response)
         .enter().append("g")
           .attr("class", "g")
-          .attr("transform", function(d) { return "translate(" + x0(d.send_weekday) + ",0)"; });
+          .attr("transform", function(d) { return "translate(" + x0(d.weekday) + ",0)"; });
 
       state.selectAll("rect")
           .data(function(d) { return d.days; })
@@ -94,7 +93,7 @@ $(document).ready(function() {
             d3.select()
             svg.append("text")
                .attr("class", "group-campaign-text")
-               .attr("transform", function() { return "translate(" + x0(d.send_weekday) +",-10)"; })
+               .attr("transform", function() { return "translate(" + x0(d.weekday) +",-10)"; })
                .attr("x", function() { return d.name; })
                .attr("dy", ".35em")
                .style("text-anchor", function() { return x1(d.name); })
